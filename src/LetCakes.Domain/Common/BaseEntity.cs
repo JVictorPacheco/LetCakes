968871769490ef83db50a101fffab07d17f77610
João Pacheco <95194054+JVictorPacheco@users.noreply.github.com>
2025-12-18 20:01:14 -0300

    Criando a minha entidade base do projeto LetCakes

---
+++ src/LetCakes.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+namespace LetCakes.Domain.Common;
+
+
+public abstract class BaseEntity
+{
+    public Guid Id {get; set;} = Guid.NewGuid(); // Unique identifier for the entity, que é gerado automaticamente ao criar uma nova instância.
+
+    public DateTime CreatedAt {get; set;} = DateTime.Now; // Data e hora de criação da entidade, que é definida automaticamente ao criar uma nova instância.
+
+    public DateTime? UpdatedAt {get; set;} // Data e hora da última atualização da entidade, que pode ser nula se a entidade nunca foi atualizada.
+
+    public bool IsActivate {get; set;} = true; // Indica se a entidade está ativa ou não, com valor padrão como true.
+    
+}
